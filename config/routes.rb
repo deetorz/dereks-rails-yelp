1a587bec3939290378f2bd0a999eca94e4afe2d9
Derek Torres <derek.n.torres@gmail.com>
2021-11-10 14:45:38 +0900

    added index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/', to: 'restaurants#index', as: 'restaurants'
-  resources :restaurants, only: %i[show new create] do
+  root to: 'restaurants#index'
+  resources :restaurants, only: %i[index show new create] do
     resources :reviews, only: %i[new create]
   end
   resources :reviews, only: [:destroy]
